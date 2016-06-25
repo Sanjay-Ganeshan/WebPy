4d2691dbc25ffa963e5648b49453d96b185f4944
Sanjay Ganeshan <sangan@live.com>
2016-06-24 22:33:34 -0400

    new file

---
+++ exampleServer.py
@@ -0,0 +1,19 @@
+import time
+import BaseHTTPServer
+import json
+
+
+
+def main(HOST_NAME, PORT_NUMBER):
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+
+if __name__ == "__main__":
+    main("localhost",1337)
